e37671e7ea909e585818d1d282996acc1c907b0a
What-I-pressed <AntonCymbaliuk.oof@gmail.com>
2024-06-17 21:51:24 +0300

    Add project files.

---
+++ js/index.js
@@ -0,0 +1,75 @@
+
+
+
+//const btnChange = document.getElementById("btnAdd");
+//const modalWin = document.getElementById("modal-dialog");
+//const userTable = document.getElementById("userTable");
+
+//const btnSave = document.getElementById("btnSave");
+//const btnClose = document.getElementById("btnClose");
+
+//if (btnChange != null) {
+//    btnChange.addEventListener("click", handleBtnChangeClick);
+//}
+
+//if (btnSave != null) {
+//    btnSave.addEventListener("click", handleBtnSaveClick);
+//}
+
+//if (btnClose != null) {
+//    btnClose.addEventListener("click", handleBtnCloseClick)
+//}
+
+//function handleBtnChangeClick() {
+//    if (modalWin != null) {
+//        modalWin.show();
+//    }
+//}
+
+//function handleBtnSaveClick() {
+//    //const tblRow = document.createElement("tr");
+//    var tblRow = userTable.insertRow(userTable.childElementCount);
+
+//    var count = tblRow.insertCell(0);
+//    var Email = tblRow.insertCell(1);
+//    var PhoneNumber = tblRow.insertCell(2);
+//    var ImageUrl = tblRow.insertCell(3);
+//    var FullName = tblRow.insertCell(4);
+
+//    count = childElementCount;
+//    Email = document.getElementById("txtEmail").ariaValueText;
+//    PhoneNumber = document.getElementById("txtPhoneNumber").ariaValueText;
+//    ImageUrl = document.getElementById("txtImageUrl").ariaValueText;
+//    FullName = document.getElementById("txtFullName").ariaValueText;
+
+//    //const txtEmail = document.getElementById("txtEmail");
+//    //const txtPhoneNumber = document.getElementById("txtPhoneNumber");
+//    //const txtImageUrl = document.getElementById("txtImageUrl");
+//    //const txtFullName = document.getElementById("txtFullName");
+
+    
+//    //var cell = document.createElement("td");
+
+//    //cell.textContent = userTable.childElementCount + 1;
+//    //tblRow.appendChild()
+
+//    //cell.textContent = txtImageUrl.ariaValueText;
+//    //tblRow.appendChild()
+
+//    //cell.textContent = txtFullName.ariaValueText;
+//    //tblRow.appendChild()
+
+//    //cell.textContent = txtEmail.ariaValueText;
+//    //tblRow.appendChild()
+
+//    //cell.textContent = txtPhoneNumber.ariaValueText;
+//    //tblRow.appendChild()
+
+//    //userTable.appendChild(tblRow);
+
+//    modalWin.style.display = "none";
+//}
+
+//function handleBtnCloseClick() {
+//    modalWin.style.display = "none";
+//}
---
+++ js/site.js
@@ -0,0 +1,6 @@
+const loadDOM = (path) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", path, false);
+    xhr.send();
+    document.write(xhr.response);
+}
